839b8e898d868978f5cf8f4d455378715a6866a4
HiMyNameIsIlNano <kappei84@gmail.com>
2020-10-08 18:30:54 +0200

    [MAIN]: Init Repo

---
+++ src/main/java/com/example/socketserver/config/RabbitMQServerConfiguration.java
@@ -0,0 +1,19 @@
+package com.example.socketserver.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
+public class RabbitMQServerConfiguration {
+
+    private final ConnectionFactory connectionFactory;
+
+    @Bean
+    public RabbitAdmin rabbitAdmin() {
+        return new RabbitAdmin(connectionFactory);
+    }
+}
---
+++ src/main/java/com/example/socketserver/push/PushNotificationService.java
@@ -0,0 +1,9 @@
+package com.example.socketserver.push;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PushNotificationService {
+
+}
+
